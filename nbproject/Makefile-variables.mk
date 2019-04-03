52fae2ba9935ac1157d9845bc3b7aea0019fc84d
svstyukov <svstyukov@users.noreply.github.com>
2019-04-03 12:13:42 +0500

    Добавил nbproject, его также нужно добавить в git, т.к. без него не компилируется

---
+++ nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=MFL-MPK.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/MFL-MPK.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=mfl-mpk.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/mfl-mpk.x.tar
